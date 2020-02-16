8396d472cde1dd33fe6177e257688b338ac77475
Seth Price <ssterling@firemail.cc>
2020-02-16 14:52:49 -0500

    Correct capitalisation in package name

--- src/main/java/net/ssterling/bukkitgreentext/BgtCommandExecutor.java
+++ src/main/java/net/ssterling/bukkitgreentext/BgtCommandExecutor.java
@@ -22,7 +22,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-package net.ssterling.BukkitGreentext;
+package net.ssterling.bukkitgreentext;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
--- src/main/java/net/ssterling/bukkitgreentext/BukkitGreentext.java
+++ src/main/java/net/ssterling/bukkitgreentext/BukkitGreentext.java
@@ -22,7 +22,7 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
  * IN THE SOFTWARE.
  */
-package net.ssterling.BukkitGreentext;
+package net.ssterling.bukkitgreentext;
 
 import java.util.HashMap;
 import java.util.UUID;
